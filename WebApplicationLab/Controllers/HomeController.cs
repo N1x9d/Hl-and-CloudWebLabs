72d7be6842a0c52d1638ed0f33bf72f3c4ff2ab4
N1x9d <nsh19021999@yandex.ru>
2022-12-07 22:37:51 +0500

    Redis пре альфа (не работает)

--- WebApplicationLab/Controllers/HomeController.cs
+++ WebApplicationLab/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System;
 
 namespace WebApplicationLab.Controllers
 {
@@ -8,17 +10,26 @@ namespace WebApplicationLab.Controllers
     public class HomeController : Controller
     {
         static CounterModel model = new CounterModel();
+        static readonly ConnectionMultiplexer _redis = ConnectionMultiplexer.Connect($"localhost:2288,password=qwerty");
+
+        public HomeController()
+        {
+            var redis = RedisStore.RedisCache;
+        }
         struct CounterModel
         {
             public int Counter;
         }
 
-        
-
+ 
         [HttpGet]
         public IActionResult Get()
         {
+            var redis = RedisStore.RedisCache;
+            var a = redis.GetType();
+            //model.Counter =  Convert.ToInt32(db.StringGet("val"));
             model.Counter += 1;
+            //db.SetAdd("val", model.Counter);
             return Ok(Json(model.Counter)); 
         }
 
