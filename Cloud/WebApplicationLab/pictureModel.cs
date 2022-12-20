033fa93442b6537b1d82e356453a17c6a64025cd
N1x9d <nsh19021999@yandex.ru>
2022-12-20 23:27:17 +0500

    Изменена структура репозитория добавлен редис все еще не работает

---
+++ Cloud/WebApplicationLab/pictureModel.cs
@@ -0,0 +1,17 @@
+﻿using AspNetCore.Yandex.ObjectStorage;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WebApplicationLab
+{
+    public class PictureModel
+    {
+        public string ImageLinc { get; set; }
+
+        public string ImageName { get; set; }
+
+        public string PrImageLinc { get; set; }
+
+        public string PrImageName { get; set; }
+    }
+}
